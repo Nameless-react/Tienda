77af4e5b7151135bfd75361a325f1586f1899a7d
Joel <joel33960gmail.com>
2023-06-13 21:14:57 -0600

    Semana 05, implementación del modelo Modelo Vista Controlador

---
+++ src/main/java/com/Tienda/categoriaServiceImpl/CategoriaServiceImpl.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Tienda.categoriaServiceImpl;
+
+import com.Tienda.dao.CategoriaDao;
+import com.Tienda.domain.Categoria;
+import com.Tienda.service.CategoriaService;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author joel
+ */
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+
+    @Autowired
+    CategoriaDao categoriaDao;
+    
+    @Override
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
+        List<Categoria> categorias = categoriaDao.findAll();
+        
+        if (activos) categorias.removeIf(categoria -> !categoria.isActivo());
+        
+        
+        return categorias;
+    }
+    
+}
