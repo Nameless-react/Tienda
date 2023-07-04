d82215992392ee2bae5cf7b5f51627049a6bf65f
Joel <joel33960gmail.com>
2023-07-04 15:16:33 -0600

    Semana 06

--- src/main/java/com/Tienda/service/CategoriaService.java
+++ src/main/java/com/Tienda/service/CategoriaService.java
@@ -13,4 +13,10 @@ import java.util.List;
  */
 public interface CategoriaService {
     public List<Categoria> getCategorias(boolean activos);
+    
+    public Categoria getCategoria(Categoria categoria);
+    
+    public void save(Categoria categoria);
+    
+    public void delete(Categoria categoria);
 }
