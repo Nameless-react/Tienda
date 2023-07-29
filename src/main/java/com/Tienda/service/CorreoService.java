8203ab2c0ea4902f130ddddb54f09bb80a19ae75
Joel <joel33960gmail.com>
2023-07-29 17:27:07 -0600

    Reposición

---
+++ src/main/java/com/Tienda/service/CorreoService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.Tienda.service;
+
+/**
+ *
+ * @author joel
+ */
+
+import jakarta.mail.MessagingException;
+
+public interface CorreoService {
+    public void enviarCorreoHtml(
+            String para, 
+            String asunto, 
+            String contenidoHtml) 
+            throws MessagingException;
+}
